1bdfe821bc6c7e1c49db8d64cf46760a31573cb7
Clidenor Issac <issaccabrall@gmail.com>
2022-01-07 08:17:06 -0300

    T2 Init

---
+++ Projects and Activities/t2_studantssubjects/src/main/java/com/issaccabral/t2_studantssubjects/T2StudantssubjectsApplication.java
@@ -0,0 +1,13 @@
+package com.issaccabral.t2_studantssubjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2StudantssubjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2StudantssubjectsApplication.class, args);
+	}
+
+}
---
+++ Projects and Activities/t2_studantssubjects/src/test/java/com/issaccabral/t2_studantssubjects/T2StudantssubjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issaccabral.t2_studantssubjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2StudantssubjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
